4570ced0d55f0e511610474de98d7a75bd160d84
Your Name <jnwanjallah@ucdavis.edu>
2024-10-03 23:58:01 +0300

    Week 2 class work

---
+++ Week 2 Intro to R.R
@@ -0,0 +1,37 @@
+Week 2 Intro to R
+
+3+4
+
+2* 5
+
+exp(1)
+exp(3)
+log(4)
+?log
+log(2,4)
+log(4,2)
+
+sqrt exp(4)
+
+mynumber<-6
+mynumber == 5
+
+mynumber = 5
+
+mynumber != 5
+mynumber>4
+mynumber<4
+
+mynumber<-7
+othernumber<--3
+
+log("a-word")
+lof_of_negative<-log(-2)
+log_of_negative
+
+elephant1_kg<-3492
+elephant2_lb<-7757
+
+elephant_lb<-elephant1_kg*2.2
+elephant2_lb>elephant1_lb
+which(myelephants==max(elephants))
