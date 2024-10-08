dd2318b993051c4f860de793eedc52d8f2e33b0d
Your Name <jnwanjallah@ucdavis.edu>
2024-10-08 14:54:22 -0700

    Home Work WK2

---
+++ home work week2.R
@@ -0,0 +1,29 @@
+
+set.seed(15)
+hw2 <- runif(50, 4, 50)
+hw2 <- replace(hw2, c(4,12,22,27), NA)
+hw2
+
+na.omit(hw2)
+
+
+hw2<-14:38
+
+Prob1<-hw2
+remove(hw2)
+
+Prob1*3
+
+times3<-Prob1*3
+
+times3 +10
+
+plus10<-times3+10
+
+final<-plus10 (true, false)
+
+remove(final<-plus10 (true, false)
+
+final<-plus10[c(TRUE,FALSE)]
+final       
+
