8652446e3013bed0ed3316efac7886a2947c4821
Your Name <jnwanjallah@ucdavis.edu>
2024-10-08 15:01:53 -0700

    homework 2

--- home work week2.R
+++ home work week2.R
@@ -4,26 +4,22 @@ hw2 <- runif(50, 4, 50)
 hw2 <- replace(hw2, c(4,12,22,27), NA)
 hw2
 
-na.omit(hw2)
+prob1 <- hw2[!is.na(hw2)]
 
 
-hw2<-14:38
+prob1 <- prob1[prob1 >14 & prob1 < 38] #only selecting numbers between 14 and 38
 
-Prob1<-hw2
-remove(hw2)
+
 
 Prob1*3
 
-times3<-Prob1*3
+times3 <- prob1 * 3
 
 times3 +10
 
 plus10<-times3+10
 
-final<-plus10 (true, false)
-
-remove(final<-plus10 (true, false)
 
 final<-plus10[c(TRUE,FALSE)]
-final       
+final
 
